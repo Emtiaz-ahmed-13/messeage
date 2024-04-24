6f275c64b8e159157b8866de6467c74cf83a34bc
emtiaz ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2024-04-24 16:01:40 +0600

    fix database error

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -7,25 +7,21 @@ type ConnectionObject ={
 
 const connection:ConnectionObject = {}
 
-async function dbConnect(): Promise<void>{
+async function dbConnect(): Promise<void> {
     //database aageiii connected ase naki checking...
-    if(connection.isConnected){
+    if (connection.isConnected) {
         console.log("Already connected to database");
-        return
+        return;
     }
-    try{
-         const db = await mongoose.connect(process.env.MONGODB_URI || '',{})
-        
-        connection.isConnected= db.connections[0].readyState
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI ||'' ,{});
+
+        connection.isConnected = db.connections[0].readyState;
 
         console.log("DB Connected Successfully");
-
-    }
-    catch(error)
-    {
-        console.log("Database connection failed ",error);
+    } catch (error) {
+        console.log("Database connection failed ", error);
         process.exit(1);
-
     }
 
 }
